5ea7c2cc7dded94ec4f8ab9b64a116a98b660338
phamdt203 <phamdt203@gmail.com>
2022-12-01 22:14:12 +0700

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    document.getElementById("pdt").innerHTML = "Paragraph changed"
+}
+document.write(5 + 6);
+window.alert("Pham Dinh Tien");
+var x = 6;
+x = 7;
+document.write(typeof undefined)
+console.log(x)
